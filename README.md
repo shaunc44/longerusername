9c4c2b0c7a6d1020a07cf3345b6e0534a50c7b58
Shaun Cox <coxs@osgbilling.com>
2020-06-25 12:28:14 -0400

    update migration to Django 1.8+ specs

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # longerusername_3
 An update to the longerusername package for Python 3 and Django 1.8+
+
+https://pypi.org/project/longerusername/#description
+
+
+
+Contact
+shauncox44 at gmail dot com
+
+
+
--- migrations/0001_initial.py
+++ migrations/0001_initial.py
@@ -1,24 +1,35 @@
-# encoding: utf-8
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+# from south.db import db
+# from south.v2 import SchemaMigration
+
+from django.db import migrations, models
 from longerusername import MAX_USERNAME_LENGTH
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Changing field 'User.username'
-        db.alter_column('auth_user', 'username', models.CharField(max_length=MAX_USERNAME_LENGTH()))
+	initial = True
+
+    dependencies = [
+    ]
+
+	operations = [
+        migrations.AlterField(
+        	model_name='auth_user',
+        	name='username',
+        	field=models.CharField(max_length=MAX_USERNAME_LENGTH()),
+        )
+    ]
+
+    # Old - South method
+    # def forwards(self, orm):
+    #     # Changing field 'User.username'
+    #     db.alter_column('auth_user', 'username', models.CharField(max_length=MAX_USERNAME_LENGTH()))
+
+    # def backwards(self, orm):
+    #     # Changing field 'User.username'
+    #     db.alter_column('auth_user', 'username', models.CharField(max_length=35))
+
+    # models = {}
+    # complete_apps = ['django_monkeypatches']
 
 
-    def backwards(self, orm):
 
-        # Changing field 'User.username'
-        db.alter_column('auth_user', 'username', models.CharField(max_length=35))
-
-
-    models = {
-        
-    }
-
-    complete_apps = ['django_monkeypatches']
