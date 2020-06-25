e7e891224575901a5d28dc61770e6343ee3fca0b
Shaun Cox <coxs@osgbilling.com>
2020-06-25 16:33:56 -0400

    add setup file

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+setuptools.setup(
+    name='longerusername_3',
+    version='1.0',
+    description='longerusername that works with Django 1.8+',
+    url='https://github.com/shaunc44/longerusername_3.git',
+    author='Skoczen; updated by Shaun Cox',
+    author_email='shauncox44 at gmail dot com',
+    license='MIT License',
+    platforms=['any'],
+    classifiers = [
+		'Environment :: Web Environment',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: MIT License',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python 3',
+		'Framework :: Django 1.8+'
+	],
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+)
+
+
+
